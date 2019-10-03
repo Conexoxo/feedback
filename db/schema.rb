3758006d263e55ee32c224592e18d61d1e0351a6
PedroConejera <pedroconejerab@gmail.com>
2019-10-03 01:29:58 -0700

    Set local code directory as a volume

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2019_10_07_082041) do
-=======
-ActiveRecord::Schema.define(version: 0) do
->>>>>>> Set db
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-<<<<<<< HEAD
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -89,6 +84,4 @@ ActiveRecord::Schema.define(version: 0) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "courses"
   add_foreign_key "comments", "users"
-=======
->>>>>>> Set db
 end

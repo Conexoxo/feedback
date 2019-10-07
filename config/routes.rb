431c19f9b67bf48ed85a6e02146365455dfa97db
PedroConejera <pedroconejerab@gmail.com>
2019-10-06 17:42:32 -0700

    Startup and registration views are created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :profesors
+  devise_for :estudiantes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
 end

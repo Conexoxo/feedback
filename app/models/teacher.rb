c3030677665beecd0183a08c7d172772b0e6de3b
Pedro Conejera <pedroconejerab@gmail.com>
2019-10-06 23:19:41 -0300

    Update teacher.rb

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,4 +1,8 @@
 class Teacher < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :teacher_courses
   has_many :courses, through: :teacher_courses
 end

ea4d8c8c36d8cfdc00024af7bd4c2b1529086bdb
GonzaloManquilef <g.manquilef.n@gmail.com>
2019-10-10 19:27:43 -0300

    Add the views and controller configuration for teachers and students profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,34 @@
 class UsersController < ApplicationController
 	helper_method :is_student?
+	before_action :set_user, only: [:show, :edit, :update]
 
 	def index
 		@courses = Course.all.sort
 		@users = User.all.sort
 		@users_count = User.all.count
 		if is_student? 
-			render "student"
+			render "student_index"
 		else
-			render "teacher"
+			render "teacher_index"
 		end
 	end
 
-  def show
+	def show
+		@teacher_courses = Course.all.sort
+		@courses_count = Course.all.count
+		if is_student? 
+			render "student_show"
+		else
+			render "teacher_show"
+		end
   end
 
   def update
 	end
 	
+	private
+
+	def set_user
+		@user = User.find(params[:id])
+	end
 end

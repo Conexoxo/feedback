b3db7de8129b4eaec326f032c5fbb87ed78ab15f
PedroConejera <pedroconejerab@gmail.com>
2019-10-16 18:21:47 -0700

    Add return button and presence validation in comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ApplicationRecord
   has_rich_text :comment
-
+  validates :comment, presence: true
+  
   belongs_to :course
   belongs_to :user
 end
--- app/models/teacher_comment.rb
+++ app/models/teacher_comment.rb
@@ -1,5 +1,6 @@
 class TeacherComment < ApplicationRecord
   has_rich_text :teacher_comment
+  validates :teacher_comment, presence: true
 
   belongs_to :user
 end
